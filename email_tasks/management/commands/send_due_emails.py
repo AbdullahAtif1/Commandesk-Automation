788c3b23ef88d2eae98a0563f8e6fe15b6e6629f
AbdullahAtif1 <abdullahatif132@gmail.com>
2025-01-23 12:34:48 +0500

    inventory_related_emails - fixed 👍
    Turns out the bug wasn't in the inventory_related_emails func, it was inside the process due emails func, directed towards the client after a sale is made

--- email_tasks/management/commands/send_due_emails.py
+++ email_tasks/management/commands/send_due_emails.py
@@ -1,7 +1,7 @@
 from django.utils.timezone import now
 from sales.models import EmailTask, execute_email_tasks
 
-def process_due_emails():
+def process_due_emails(task=None):
     """Standalone function to process and send due email tasks."""
     due_email_tasks = EmailTask.objects.filter(sent=False, time_to_send__lte=now())
     if due_email_tasks.exists():
